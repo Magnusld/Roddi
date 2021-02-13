7aeaf7552c19f87ce0404b5045fe05069420b516
yngvetl <yngvetl@stud.ntnu.no>
2021-02-13 19:39:44 +0100

    La til samarbeid mellom Django og Vue

--- roddivue/src/App.vue
+++ roddivue/src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  {{ notes }}
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios'
 
 @Options({
   components: {
     HelloWorld,
   },
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+  name: 'App' | undefined;
+  notes: undefined;
+
+  mounted(){
+    axios.get('http://localhost:8000/api/notes/').then((response) => {
+      this.notes = response.data
+      console.log(this.data)
+  })
+}
+
+}
+
+
+
+
 </script>
 
 <style>
