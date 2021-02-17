856913d0badf482d4c709e87e854f5eef5afc58c
yngvetl <yngvetl@stud.ntnu.no>
2021-02-17 15:55:20 +0100

    feat(#5): Legge til djoser

--- roddidjango/roddi/urls.py
+++ roddidjango/roddi/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.notes.urls')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
