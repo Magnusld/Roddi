8f005a9719bda1c3b339472b97471a4851ea5aa6
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-02-23 10:26:15 +0100

    fix(#9) endret null=True til blank=True for opprettelse av Estate

--- roddidjango/apps/estate/models.py
+++ roddidjango/apps/estate/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Estate(models.Model):
     name = models.CharField(max_length=255)
-    participants = models.ManyToManyField(to=User, related_name='users', null=True)
+    participants = models.ManyToManyField(to=User, related_name='users', blank=True)
 
     def __str__(self):
         return self.name
