5d32505485c808c8fb8d3a541997bb4edb447349
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-02-23 08:24:48 +0100

    feat(#9) legge til views og urls for dødsbo

--- roddidjango/apps/estate/views.py
+++ roddidjango/apps/estate/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import EstateSerializer, EstateItemSerializer
+from .models import Estate, EstateItem
+
+class EstateViewSet(viewsets.ModelViewSet):
+    serializer_class = EstateSerializer
+    queryset = Estate.objects.all()
+
+
+class EstateItemViewSet(viewsets.ModelViewSet):
+    serializer_class = EstateItemSerializer
+    queryset = EstateItem.objects.all()
