df17ffbdba5684bf7a84b9c65734bf6907b46879
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-03-15 16:43:08 +0100

    feat(#26,#30,#33): legge inn start på logikk for endring/sletting av relasjoner mellom bruker og items

--- roddidjango/apps/estate/admin.py
+++ roddidjango/apps/estate/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Estate, EstateItem, ItemPriorityInline, ItemVoteInline
+from .models import Estate, EstateItem, ItemPriority, ItemVote, ItemPriorityInline, ItemVoteInline
 
 class EstateItemAdmin(admin.ModelAdmin):
     inlines = (ItemVoteInline,ItemPriorityInline)
 
 admin.site.register(Estate)
 admin.site.register(EstateItem,EstateItemAdmin)
+admin.site.register(ItemPriority)
+admin.site.register(ItemVote)
--- roddidjango/apps/estate/views.py
+++ roddidjango/apps/estate/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import EstateSerializer, EstateItemSerializer, ItemVoteSerializer, ItemPrioritySerializer
 from .models import Estate, EstateItem, ItemVote, ItemPriority
@@ -22,13 +24,17 @@ class ItemVoteViewSet(viewsets.ModelViewSet):
     queryset = ItemVote.objects.all()
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.query_params.get(key='userID', default=None),
+        return self.queryset.filter(user=self.request.user.id,
                                     item=self.request.query_params.get(key='itemID', default=None))
 
 class ItemPriorityViewSet(viewsets.ModelViewSet):
     serializer_class = ItemPrioritySerializer
     queryset = ItemPriority.objects.all()
-                                                                                                            
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.query_params.get(key='userID', default=None),
+        return self.queryset.filter(user=self.request.user.id,
                                     item=self.request.query_params.get(key='itemID', default=None))
+
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     self.perform_destroy(instance)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
