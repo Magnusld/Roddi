af40c2715ea2f5a5d84243b15db218ce58f56725
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-03-16 11:12:25 +0100

    feat(#26,#30,#33): legge inn optional filtrering for itemvotes og itempriority, oppdatere README.md med relevante API-calls

--- roddidjango/apps/estate/views.py
+++ roddidjango/apps/estate/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework import status
 
 from .serializers import EstateSerializer, EstateItemSerializer, ItemVoteSerializer, ItemPrioritySerializer
 from .models import Estate, EstateItem, ItemVote, ItemPriority
+
 
 class EstateViewSet(viewsets.ModelViewSet):
     serializer_class = EstateSerializer
@@ -19,22 +16,28 @@ class EstateItemViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(belongs_to=self.request.query_params.get(key='estateID', default=None))
 
+
 class ItemVoteViewSet(viewsets.ModelViewSet):
     serializer_class = ItemVoteSerializer
     queryset = ItemVote.objects.all()
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user.id,
-                                    item=self.request.query_params.get(key='itemID', default=None))
+        item_id = self.request.query_params.get(key='itemID', default=None)
+        if item_id is None:
+            return self.queryset.all()
+        else:
+            return self.queryset.all().filter(user=self.request.user.id,
+                                              item=self.request.query_params.get(key='itemID', default=None))
+
 
 class ItemPriorityViewSet(viewsets.ModelViewSet):
     serializer_class = ItemPrioritySerializer
     queryset = ItemPriority.objects.all()
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user.id,
-                                    item=self.request.query_params.get(key='itemID', default=None))
 
-    # def destroy(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     self.perform_destroy(instance)
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_queryset(self):
+        item_id = self.request.query_params.get(key='itemID', default=None)
+        if item_id is None:
+            return self.queryset.all()
+        else:
+            return self.queryset.all().filter(user=self.request.user.id,
+                                              item=self.request.query_params.get(key='itemID', default=None))
