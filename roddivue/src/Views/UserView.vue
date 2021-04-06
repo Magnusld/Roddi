8f5ddee21460c4a46bdaf7115e2e5c7035f843a3
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2021-04-06 09:00:24 +0200

    fix(): Legge til fixplz metode

--- roddivue/src/Views/UserView.vue
+++ roddivue/src/Views/UserView.vue
@@ -1,15 +1,35 @@
 <template>
   <UserPage />
+  <Button @click="fixPlz">
+
+  </Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 import UserPage from "@/components/UserPage.vue"
+import Button from "primevue/button";
+import {store} from "@/store";
+import router from "@/router";
 
 export default defineComponent({
   name: "UserView",
   components: {
-    UserPage
+    UserPage,
+    Button
+  },
+  methods: {
+    onClick() {
+      console.log(this.$store.getters.getLoggedInStatus)
+    },
+    fixPlz() {
+      localStorage.removeItem("token")
+      store.commit('removeToken')
+      store.commit('removeUsername')
+      store.commit('removeUserID')
+      router.push('/login')
+      this.onClick()
+    }
   }
 })
 </script>
