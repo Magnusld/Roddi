16a3863649d7aa1e0e9ec21840878c384661eefc
Bjørn Are Odden <bjorao@stud.ntnu.no>
2021-04-06 09:10:26 +0200

    feat(#52): legge inn adminstatus i api. Endre viewsets for bruker til readonly

--- roddidjango/users/views.py
+++ roddidjango/users/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
-
 # Create your views here.
-from .models import User
 from rest_framework import viewsets
-from django.conf import settings
 
-from .serializers import UserIdSerializer
+from .models import User
+from .serializers import UserIdSerializer, UserIsAdminSerializer
 
 
-class UserIdViewSet(viewsets.ModelViewSet):
+class UserIdViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserIdSerializer
 
     def get_queryset(self):
         return User.objects.filter(id=self.request.user.id)
 
+
+class UserIsAdminViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserIsAdminSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
